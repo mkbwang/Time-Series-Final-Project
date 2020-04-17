7876ca57d3ba50de4737a138b14f5342446360d0
wangmk <wangmk@umich.edu>
2020-04-17 15:44:10 -0400

    Updating the MIF code to add log likelihood evaluation.

--- eagle_mif_v1.R
+++ eagle_mif_v1.R
@@ -1,16 +1,18 @@
 library(pomp)
 library(doParallel)
-registerDoParallel()
-library(doRNG)
-registerDoRNG(3899882)
+set.seed(2020,kind="L'Ecuyer")
+cores <-  as.numeric(Sys.getenv('SLURM_NTASKS_PER_NODE', unset='8'))
+cl <- makeCluster(cores)
+registerDoParallel(cl)
 
+setwd(getwd())
 # setwd("~/UM/Biostatistics/Courses/STATS531/Time-Series-Final-Project")
 
 source("eagle_model_v1.R")
 
 run_level <- 2
 switch(run_level, {
-  eagle_Np=100; eagle_Nmif=10; bsflu_Neval=10;
+  eagle_Np=100; eagle_Nmif=10; eagle_Neval=10;
   eagle_Nglobal=10; eagle_Nlocal=10
 },{
   eagle_Np=20000; eagle_Nmif=100; eagle_Neval=10;
@@ -22,8 +24,6 @@ switch(run_level, {
 
 # set starting parameters for local search
 
-data = read.csv("https://raw.githubusercontent.com/skybullbobby/Time-Series-Final-Project/master/eagle_421.csv")
-
 fit.gamma <- fitdist(data$msa, distr = "gamma", method = "mle")
 startshape <- fit.gamma$estimate[[1]]
 startscale <- 1/fit.gamma$estimate[[2]] 
@@ -31,19 +31,19 @@ startscale <- 1/fit.gamma$estimate[[2]]
 eagle_startval <- c(p0=0.6, p1=0.4, shape0=startshape, scale0=startscale,
                     shape1=startshape, scale1=startscale)
 
-
+# box for global search
 eagle_box <- rbind(
   p0=c(-2,-0.1),
   p1=c(-2,-0.1),
-  shape0=c(-1.5, -0.5),
-  scale0=c(-1.5, -0.5),
-  shape1=c(-1.5, -0.5),
-  scale1=c(-1.5, -0.5)
+  shape0=c(-1.5, -0.1),
+  scale0=c(-1.5, -0.1),
+  shape1=c(-1.5, -0.1),
+  scale1=c(-1.5, -0.1)
 )
-
 
 eagle_rw.sd <- 0.02; eagle_cooling.fraction.50 <- 0.5
 
+# Iterative filtering
 stew(file=sprintf("global_search-v1-%d.rda",run_level),{
   t_global <- system.time({
     mifs_global <- foreach(i=1:eagle_Nglobal,
@@ -65,3 +65,16 @@ stew(file=sprintf("global_search-v1-%d.rda",run_level),{
   })
 },seed=12345,kind="L'Ecuyer")
 
+
+# log likelihood
+stew(file=sprintf("lik_global_eval-v1-%d.rda",run_level),{
+  t_global_eval <- system.time({
+    liks_global <- foreach(i=1:eagle_Nglobal,
+                           .combine=rbind, .packages='pomp') %dopar% {
+                             evals <- replicate(eagle_Neval,
+                                                logLik(pfilter(eagle_0,
+                                                               params=coef(mifs_global[[i]]),Np=eagle_Np)))
+                             logmeanexp(evals, se=TRUE)
+                           }
+  })
+},seed=56789,kind="L'Ecuyer")
